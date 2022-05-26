542f870357bf33b312bbced70e42acc0859bdc8b
Elias <emendoza@fi.uba.ar>
2022-05-26 02:09:00 -0300

    Se agrega la implementación del tablero con listas

--- tablero.hpp
+++ tablero.hpp
@@ -1,12 +1,30 @@
 #ifndef TABLERO_H_
 #define TABLERO_H_
 
+#include "lista.hpp"
+#include "casillero.hpp"
 
 class Tablero{
 private:
-    int turno;
+    int largo;
+    int ancho;
+    int alto;
     //Cubo de listas
+    //Puntero a la estructura de casilleros
+    Lista<Lista<Lista<Casillero*>*>*>* tablero; 
+
+    int turno;
+public:
+    Tablero(int largo, int ancho, int alto);
+    ~Tablero();
+    int getLargo();
+    int getAncho();
+    int getAlto();
+    //POST: devuelve un puntero a casillero según las coordenadas que se indiquen
+    Casillero* getCasillero(int largo, int ancho, int alto);
 };
 
 
-#endif 
+
+
+#endif /* TABLERO_H_ */

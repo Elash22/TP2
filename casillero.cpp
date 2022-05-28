8f12c7c76d6dcadc36258786073cb260b0387b5e
SebastianBranda <sebastianbranda@gmail.com>
2022-05-28 17:35:05 -0300

    implementacion clase Casillero

--- casillero.cpp
+++ casillero.cpp
@@ -1,13 +1,50 @@
+#include <iostream>
 #include "casillero.hpp"
 #include "coordenada.hpp"
-#include <iostream>
 
-using namespace std;
-
-Casillero::Casillero(int fila, int columna, int altura, TipoTerreno terreno){
-    this->posicion->setFila(fila);
-    this->posicion->setColumna(columna);
-    this->posicion->setAltura(altura);
-    this->tipoTerreno = terreno;
+Casillero::Casillero(int fila, int columna, int altura, TipoTerreno tipo){
+    this->posicion = new Coordenada(fila, columna, altura);
+    this->estado = vacia;
+    this->tipoTerreno = tipo;
+    this->jugador = NULL;
+    this->unidad = NULL;
 }
 
+Coordenada* Casillero::getCoordenada(){
+    return this->posicion;
+}
+
+EstadoDeCasillero Casillero::getEstado(){
+    return this->estado;
+}
+
+void Casillero::setEstado(EstadoDeCasillero e){
+    this->estado = e;
+    return;
+}
+
+TipoTerreno Casillero::getTipoDeTerreno(){
+    return this->tipoTerreno;
+}
+
+void Casillero::setJugador(Jugador* jugador){
+    if(jugador != NULL){
+        this->jugador = jugador;
+    }
+    return;
+}
+
+Jugador* Casillero::getJugador(){
+    return this->jugador;
+}
+
+void Casillero::setUnidad(Unidad* unidad){
+    if(unidad != NULL){
+        this->unidad = unidad;
+    }
+    return;
+}
+
+Casillero::~Casillero(){
+    delete(this->posicion);
+}
